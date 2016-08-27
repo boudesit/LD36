59cab8e94b849c2d0157b921e120c89e8d283fdd
Quentin <quentindegraeve59@gmail.com>
2016-08-27 17:46:57 +0200

    colision

--- js/ColisionManager.js
+++ js/ColisionManager.js
@@ -1,14 +1,25 @@
-var ColisionManager = function(game) {
+function ColisionManager(game) {
+	this.game = game ;
+};
 
-}
+ColisionManager.prototype.create = function create() {
+	explosion = game.add.audio('explosion');
+	hit = game.add.audio('hit');
+	screenShake = game.add.audio('screenShake');
+};
 
-ColisionManager.prototype = {
-    create: function() {
+ColisionManager.prototype.ennemyHitHero = function ennemyHitHero(player,ennemy) {
 
-    },
+	return true;	
+};
 
-    update: function() {
 
-    }
+ColisionManager.prototype.fireHitEnnemy = function Colision(hit,ennemy) {
 
-}
+
+};
+
+ColisionManager.prototype.update = function update() {
+	
+};
+
