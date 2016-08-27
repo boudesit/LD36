a22dc95bb864e5760738a8fabb300ec1756940d8
Quentin <quentindegraeve59@gmail.com>
2016-08-28 01:15:27 +0200

    hit

--- js/ColisionManager.js
+++ js/ColisionManager.js
@@ -1,25 +1,25 @@
 function ColisionManager(game) {
 	this.game = game ;
+
 };
 
 ColisionManager.prototype.create = function create() {
+
+	/*
 	explosion = game.add.audio('explosion');
 	hit = game.add.audio('hit');
 	screenShake = game.add.audio('screenShake');
+	*/
 };
 
 ColisionManager.prototype.ennemyHitHero = function ennemyHitHero(player,ennemy) {
 
-/*	var explosion = explosions.getFirstExists(false);
-    explosion.reset(player.body.x, player.body.y);
-    explosion.play('explosion', 30, false, true);
-*/	return true;	
+  return true;	
 };
 
 
-ColisionManager.prototype.fireHitEnnemy = function Colision(hit,ennemy) {
-
-
+ColisionManager.prototype.fireHitEnnemy = function Colision(fire,ennemy) {
+  return true;
 };
 
 ColisionManager.prototype.update = function update() {
