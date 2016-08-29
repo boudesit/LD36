073835df6a70f86050c3a744cd2ba86179eff52b
boudesit <boudesit@gmail.com>
2016-08-29 20:43:56 +0200

    sprite psrite sripte !!!

--- js/gametitle.js
+++ js/gametitle.js
@@ -5,7 +5,9 @@ gameTitle.prototype = {
 
   		introSound = game.add.audio('introSound');
 
-  		this.game.add.sprite(0,0,"gametitle");
+      this.spriteBG = this.game.add.tileSprite(0, 0, 800, 600, 'gametitle');
+      this.spriteBG.animations.add('gametitle');
+      this.spriteBG.animations.play('gametitle', 2, true);
 
   		this.startButton = this.game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
 		this.startButton.onDown.add(this.playTheGame, this);
